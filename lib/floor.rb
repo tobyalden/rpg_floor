a52371715f53638ef09e0095eb553f956b08eba4
Toby <Alden@Tobys-MacBook-Pro.local>
2015-05-19 10:24:03 -0700

    Add create_boundaries method. Pass spec.

--- lib/floor.rb
+++ lib/floor.rb
@@ -11,4 +11,14 @@ class Floor
     end
   end
 
+  def create_boundaries
+    @map.each_index() do |x|
+      @map[x].each_index() do |y|
+        if(x == 0 || y == 0 || x == @width-1 || y == @height-1)
+          @map[x][y] = true
+        end
+      end
+    end
+  end
+
 end
